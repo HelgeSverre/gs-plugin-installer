0475f152dd23fdd23206e11cfd9423f2bec2eac8
helgesverre <email@helgesverre.com>
2015-07-01 19:46:24 +0200

    added shift-hold-click-selection feature to plugin list

--- gs_plugin_installer/js/script.js
+++ gs_plugin_installer/js/script.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
 
+    // Source: https://gist.github.com/DelvarWorld/3784055
+    $.fn.shiftSelectable = function() {
+        var lastChecked,
+            $boxes = this;
 
+        $boxes.click(function(evt) {
+            if(!lastChecked) {
+                lastChecked = this;
+                return;
+            }
 
+            if(evt.shiftKey) {
+                var start = $boxes.index(this),
+                    end = $boxes.index(lastChecked);
+                $boxes.slice(Math.min(start, end), Math.max(start, end) + 1)
+                    .attr('checked', lastChecked.checked)
+                    .trigger('change');
+            }
+
+            lastChecked = this;
+        });
+    };
+
+    $("#gs_plugin_form").find('input[type="checkbox"]').shiftSelectable();
 
     $('#plugin_table').DataTable({
         "columnDefs": [
