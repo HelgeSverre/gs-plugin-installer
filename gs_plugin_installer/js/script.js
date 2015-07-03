d040aaa92dbbb74c6760e31ef48daa2c69c69912
Helge Sverre <email@helgesverre.com>
2015-07-03 14:32:11 +0200

    added confirmation box when uninstalling plugins

--- gs_plugin_installer/js/script.js
+++ gs_plugin_installer/js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+
+    // Confirm when uninstalling plugins
+    $("#uninstall").click(function() { return confirm("Are you sure you want to uninstall the selected plugins?"); });
+    $("a.cancel").click(function() { return confirm("Are you sure you want to uninstall this plugin?"); });
+
+    // Initialize DataTable
     $('#plugin_table').DataTable({
         "columnDefs": [
             {
