e7d04373a38327edf5595b2baaea66727bdf5edd
Helge Sverre Hessevik Liseth <email@helgesverre.com>
2015-07-04 11:54:53 +0200

    German translation added, thanks to Andreas (df8oe)

---
+++ gs_plugin_installer/lang/de_DE.php
@@ -0,0 +1,19 @@
+<?php
+
+$i18n = array(
+    'CACHE_REFRESHED'       => 'Plugin cache wurde aktualisiert.',
+    'INSTALLED_SUCCESS'     => ' Plugins wurden erfolgreich installiert!', // number of plugins will be prepended to this string
+    'INSTALLED_FAIL'        => 'Plugin Installation fehlgeschlagen!',
+    'UNINSTALLED_SUCCESS'   => ' Plugins erfolgreich deinstalliert!', // number of plugins will be prepended to this string
+    'UNINSTALLED_FAIL'      => 'Plugin Deinstallation fehlgeschlagen!',
+    'REFRESH'               => 'Neu laden',
+    'INSTALL'               => 'Installieren',
+    'UNINSTALL'             => 'Entfernen',
+    'LIST_UPDATED'          => 'Neu geladen',
+    'LIST_PLUGIN'           => 'Plugin',
+    'LIST_DESCRIPTION'      => 'Beschreibung',
+    'LIST_INSTALL'          => 'Installiert',
+    'AUTHOR'                => 'Autor',
+    'VERSION'               => 'Version',
+    'PLUGIN_NAME'           => 'GS Plugin Installer'
+);
