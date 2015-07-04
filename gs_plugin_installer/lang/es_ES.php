96aa57d341e94f1809cad819f500446f401c689b
Helge Sverre Hessevik Liseth <email@helgesverre.com>
2015-07-04 11:56:40 +0200

    Spanish translation added, thanks to Carlos

---
+++ gs_plugin_installer/lang/es_ES.php
@@ -0,0 +1,19 @@
+<?php
+
+$i18n = array(
+    'CACHE_REFRESHED'       => 'Se ha actualizado el caché de plugins',
+    'INSTALLED_SUCCESS'     => ' plugins instalados con éxito!', // number of plugins will be prepended to this string
+    'INSTALLED_FAIL'        => '¡Instalación de plugin fallida!',
+    'UNINSTALLED_SUCCESS'   => ' plugins desinstalados con éxito!', // number of plugins will be prepended to this string
+    'UNINSTALLED_FAIL'      => 'Desinstalación de plugin fallida!',
+    'REFRESH'               => 'Actualizar lista',
+    'INSTALL'               => 'Instalar',
+    'UNINSTALL'             => 'Desinstalar',
+    'LIST_UPDATED'          => 'Actualizado',
+    'LIST_PLUGIN'           => 'Plugin',
+    'LIST_DESCRIPTION'      => 'Descripción',
+    'LIST_INSTALL'          => 'Instalar',
+    'AUTHOR'                => 'Autor',
+    'VERSION'               => 'Versión',
+    'PLUGIN_NAME'           => 'GS Plugin Installer'
+);
