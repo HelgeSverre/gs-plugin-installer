2451f9bb04985a463942cbebea8943fe56b6c3ae
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2015-07-23 00:51:42 +0200

    Add nl_NL language

---
+++ gs_plugin_installer/lang/nl_NL.php
@@ -0,0 +1,18 @@
+<?php
+$i18n = array(
+    'CACHE_REFRESHED'       => 'Plugin cache herladen',
+    'INSTALLED_SUCCESS'     => ' plugins zijn geïnstalleerd!', // number of plugins will be prepended to this string
+    'INSTALLED_FAIL'        => 'Plugin installateren mislukt!',
+    'UNINSTALLED_SUCCESS'   => ' plugins zijn verwijderd!', // number of plugins will be prepended to this string
+    'UNINSTALLED_FAIL'      => 'Plugin verwijderen mislukt!',
+    'REFRESH'               => 'Herladen',
+    'INSTALL'               => 'Installeren',
+    'UNINSTALL'             => 'Verwijderen',
+    'LIST_UPDATED'          => 'Geupdated',
+    'LIST_PLUGIN'           => 'Plugin',
+    'LIST_DESCRIPTION'      => 'Beschrijving',
+    'LIST_INSTALL'          => 'Installeren',
+    'AUTHOR'                => 'Auteur',
+    'VERSION'               => 'Versie',
+    'PLUGIN_NAME'           => 'GS Plugin Installer'
+);
